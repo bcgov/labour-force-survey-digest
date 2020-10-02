8ee36aed95019ca27f0f453db5388a5f91dab4d8
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2020-10-01 22:04:01 -0700

    more to scratch

--- scratch/scratch.R
+++ scratch/scratch.R
@@ -140,3 +140,76 @@ valueBox(value = year_change_unemployment %>%
          caption = paste("Year-Over-Year Change in B.C. Unemployment"))
 
 
+### Month-Over-Month Change in Number of Jobs by Province in `r report_month`
+
+# lim_mom_jobs_can <- c(
+#   0,
+#   lfc_province_tidy %>%
+#     ungroup() %>%
+#     filter(
+#       labour_force_characteristics == "Employment",
+#       sex == "Both sexes",
+#       age_group == "15 years and over",
+#       date == max(date),
+#       geo != "Canada"
+#     ) %>%
+#     slice_max(month_change, n = 1) %>%
+#     pull(month_change) + 30000
+# )
+
+mom_change_employment_provinces <- lfc_province_tidy %>%
+  filter(
+    labour_force_characteristics == "Employment",
+    sex == "Both sexes",
+    age_group == "15 years and over",
+    date == max(date),
+    geo != "Canada") %>%
+  mutate(fill_col = ifelse(geo == "British Columbia", "bc", "other")) %>%
+  ggplot(aes(x = reorder(geo, -month_change), y = month_change)) +
+  geom_col(aes(fill = fill_col,
+              text = paste(
+      "Month-Over-Month Change:",
+      signs(
+       month_change,
+       format = comma,
+       add_plusses = TRUE),
+      "<br>",
+      "% Month-Over-Month Change:",
+      signs(
+        month_change_percent,
+        format = percent,
+        add_plusses = TRUE,
+        accuracy = 0.1
+      )
+    )), alpha = 0.6) +
+  coord_flip() +
+  geom_text(
+    aes(
+      label = signs(
+        month_change_percent,
+        format = percent,
+        add_plusses = TRUE,
+        accuracy = 0.1
+      )
+    ),
+    nudge_y = 8000,
+    colour = "grey20",
+    size = 2.5
+  ) +
+  labs(x = NULL,
+       y = NULL) +
+  scale_y_continuous(
+    labels = comma,
+    expand = c(0, 0),
+    # limits = lim_mom_jobs_can,
+    breaks = breaks_pretty(n = 6)
+  ) +
+  scale_fill_manual(guide = FALSE,
+                    values = c(bc = "#440154FF",
+                               other = main_colour)) +
+  theme_minimal() +
+  theme_horiz_bar +
+  theme(legend.position = "none")
+
+ggplotly_lfs(mom_change_employment_provinces)
+
